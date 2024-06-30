cfd3306a8e497344bd55c9d5ecb7ec63a2f069cf
Kubai James Karomo <129506433+kj728@users.noreply.github.com>
2024-06-30 09:05:18 +0300

    Subsequent commit

--- Triptide Angular/TripTideTours/src/app/bookings-view/bookings-view.component.ts
+++ Triptide Angular/TripTideTours/src/app/bookings-view/bookings-view.component.ts
@@ -8,7 +8,9 @@ import { ToursService } from '../Services/Tour/tours.service';
 import { IBooking } from '../Models Angular/Booking';
 import { IUser, Payload } from '../Models Angular/User';
 import { AuthenticationService } from '../Services/Authentication/authentication.service';
-import { number } from 'joi';
+
+import { ITour } from '../Models Angular/Tours';
+import { IHotel } from '../Models Angular/Hotel';
 
 @Component({
   selector: 'app-bookings-view',
@@ -19,15 +21,19 @@ import { number } from 'joi';
 })
 export class BookingsViewComponent implements OnInit {
 
-  usersBookings: IBooking[] = []
+  // usersBookings: IBooking[] = []
+  usersBookings: IBooking[] = [];
   user!: Payload
   userId!: string
   tourId!: string
-  tourName: string = ' '
-  tourDestination!: string
-  // userDeets!: IUser
   userName: string = ''
+  allHotels: IHotel[] = []
+  allTours: ITour[] = []
+  count: number = 1
 
+  bookedTours: string[] = [];
+  bookedHotels: string[] = [];
+  toursPrice: string[] = [];
 
 
   constructor(
@@ -36,74 +42,94 @@ export class BookingsViewComponent implements OnInit {
     private hotelService: HotelService,
     private bookingService: BookingService,
     private authService: AuthenticationService
+  ) { }
 
-  ) {
-
-  }
   ngOnInit(): void {
     const userString = localStorage.getItem('currentUser');
     if (userString) {
       this.user = JSON.parse(userString);
     }
 
-    this.bookingService.getAllBookingsUser(this.user.sub).subscribe(userBookingsArray => {
-      //console.log(userBookingsArray)
-      this.usersBookings = userBookingsArray;
-      // Access tourId and hotelId for each booking
-      this.usersBookings.forEach(booking => {
-        const tourId = booking.tourid;
-        const hotelId = booking.hotelid;
-        console.log("Tour ID:", tourId);
-        console.log("Hotel ID:", hotelId);
-
-        this.toursService.getAllTours().subscribe(res => {
-          console.log(res)
-          // this.tourName = tour.tourname
-          // this.tourDestination = tour.tdestination
-        })
-
-
-
-
-
-      });
-
-    })
-
     this.userId = this.user.sub
     console.log(this.userId)
 
-    this.authService.getSpecificUser(this.userId).subscribe(res => {
-      console.log(res)
-      this.userName = res.username;
+    this.authService.getSpecificUser(this.userId).subscribe(specificUser => {
+      // console.log(res)
+      this.userName = specificUser.username;
     })
 
+    this.bookingService.getAllBookingsUser(this.userId).subscribe(userBookingsArray => {
+      this.usersBookings = userBookingsArray;
+      this.bookedTours = this.usersBookings.map(booking => this.getBookedTourName(booking.tourid));
+      this.bookedHotels = this.usersBookings.map(booking => this.getBookedHotelName(booking.hotelid));
+      this.toursPrice = this.usersBookings.map(booking => this.getBookedTourPrice(booking.tourid));
+    });
 
+    this.hotelService.getAllHotels().subscribe(allHotelsArray => {
+      this.allHotels = allHotelsArray;
+      console.log(allHotelsArray)
+    });
 
-
-    // this.toursService.getSpecificTour(this.tourId).subscribe(res => {
-    //   console.log(res)
-    //   // this.tourName = tour.tourname
-    //   // this.tourDestination = tour.tdestination
-    // })
-
-
-
-
-
-
+    this.toursService.getAllTours().subscribe(allToursArray => {
+      this.allTours = allToursArray;
+      console.log(allToursArray)
+    });
   }
 
-  getTour(id: string): number {
-    let tourPrice!: number;
-    console.log("Getting Tour:", id)
-    this.toursService.getSpecificTour(id).subscribe(tour => {
-      // console.log(tour)
-      // this.tourName = tour.tourname
-      // this.tourDestination = tour.tdestination
+  // getBookedTour(id: string): ITour {
+  //   let tour!: ITour;
+  //   console.log("Getting Tour:", id)
+  //   this.toursService.getSpecificTour(id).subscribe(tour => {
+  //     console.log(tour)
+  //     tour = tour;
+  //   })
+  //   return tour;
+  // }
 
-      tourPrice = tour.tprice
-    })
+  //   getBookedTour(id: string) {
+  //     const tour = this.allTours.find(t => t.id === id)
+  //     console.log(tour)
+  //     // if (!tour) {
+  //     //   return null;
+  //     // }
+  //     // return tour;
+
+
+  // }
+
+
+  getBookedTourName(id: string): string {
+    let tourName = '';
+    const tour = this.allTours.find(t => t.id === id)
+    if(tour){
+      tourName=tour.tourname
+    }
+    return tourName;
+  }
+
+  getBookedHotelName(hotelid: string): string {
+    let hotelName = '';
+    const hotel = this.allHotels.find(h => h.id === hotelid)
+    if(hotel){
+      hotelName=hotel.hotelname
+    }
+    return hotelName;
+  }
+
+  
+  getBookedTourPrice(tourid: string): any {
+    let tourPrice = 0;
+    const tour = this.allTours.find(t => t.id === tourid)
+    if(tour){
+      tourPrice=tour.tprice
+    }
     return tourPrice;
   }
+  
+
+
+
 }
+
+
+
